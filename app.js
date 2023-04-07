560f57d4faa48ecef697937d70d0b0037b732a0a
Godwin-NJ <amadigodwin7@gmail.com>
2023-04-07 21:39:42 +0100

    graphql tut first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const graphqlHTTP = require("express-graphql").graphqlHTTP;
+const schema = require("./schemaType/schema");
+const mongoose = require("mongoose");
+// express server initialization
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  })
+);
+
+const connectApp = async () => {
+  try {
+    const db = await mongoose.connect(
+      `mongodb+srv://${process.env.MONGOUSER}:${process.env.MONGOPWD}@cluster0.w6ddver.mongodb.net/?retryWrites=true&w=majority`,
+      {
+        dbName: "graphqlDB",
+        serverSelectionTimeoutMS: 5000,
+      }
+    );
+    if (db) {
+      app.listen(PORT, () => {
+        console.log(`app listening on port ${PORT}`);
+      });
+    }
+  } catch (error) {
+    console.log(`connecting to database failed with error : ${error}`);
+  }
+};
+
+connectApp();
